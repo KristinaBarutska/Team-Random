c79509d748ab60aec120fc7df3e2a93d99e77da6
KristinaBarutska <KristinaBarutska@users.noreply.github.com>
2016-12-21 14:56:20 +0200

    Update recept-details.component.ts

--- app/recept-details.component.ts
+++ app/recept-details.component.ts
@@ -1,1 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Recept } from './Recept';
 
+@Component({
+  selector: 'my-recept-detail',
+  template: `
+  <div *ngIf="recept">
+    <h2>{{recept.title}} details!</h2>
+    <div><label>id: </label>{{recept.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="recept.title" placeholder="title"/>
+    </div>
+  </div>
+`
+})
+export class ReceptDetailComponent {
+    @Input()
+    recept: Recept;
+}

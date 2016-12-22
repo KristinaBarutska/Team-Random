64a3ddd6b30aeef0d7f69f6e0bc0baf80a8b07b0
KristinaBarutska <KristinaBarutska@users.noreply.github.com>
2016-12-22 19:35:58 +0200

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { ReceptDetailComponent } from './recept-details.component';
-
-
+import { ArticlesComponent } from './articles.component';
+import { ArticleDetailComponent } from './article-details.component';
+import { DashboardComponent } from './dashboard.component';
+import { ArticleService } from './article.service';
+import { AppRoutingModule }     from './app-routing.module';
+ 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
+    
     AppComponent,
-    ReceptDetailComponent
+    ArticlesComponent,
+    DashboardComponent,
+    ArticleDetailComponent
   ],
+  providers:[ArticleService],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }

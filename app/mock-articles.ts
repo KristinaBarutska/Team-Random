5fbd07a6b8d85905b89d09f4239387860b843135
KristinaBarutska <KristinaBarutska@users.noreply.github.com>
2016-12-22 19:55:45 +0200

    Update and rename mock-recepts.ts to mock-articles.ts

---
+++ app/mock-articles.ts
@@ -0,0 +1,9 @@
+import { Article } from './Article';
+
+export const ARTICLES: Article[] = [
+  { id: 1, title: 'Хан Тенгри' },
+  { id: 2, title: 'Монблан' },
+  { id: 3, title: 'Тур Дон Кихот' },
+  { id: 4, title: 'Аконкагуа' },
+  { id: 5, title: 'Манаслу' }
+];
--- app/mock-recepts.ts
+++
@@ -1,9 +0,0 @@
-import { Recept } from './Recept';
-
-export const RECEPTS: Recept[] = [
-  { id: 1, title: 'Боб по ловджийски' },
-  { id: 2, title: 'Спагети болонезе' },
-  { id: 3, title: 'Картофи по родопски' },
-  { id: 4, title: 'Телешко варено' },
-  { id: 5, title: 'Яйца по панагюрски' }
-];
